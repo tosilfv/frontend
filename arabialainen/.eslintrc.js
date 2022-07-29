f9c504bf17636158bc0d52cc446c4ece5393499a
Toni Silfver <toni.silfver@protonmail.ch>
2022-07-29 12:27:50 +0300

    v8 mod .eslintrc.js

--- arabialainen/.eslintrc.js
+++ arabialainen/.eslintrc.js
@@ -4,8 +4,7 @@ module.exports = {
     'node': true,
     'browser': true,
     'es6': true,
-    'jest/globals': true,
-    'cypress/globals': true
+    'jest/globals': true
   },
   'extends': [
     'eslint:recommended',
@@ -19,7 +18,7 @@ module.exports = {
     'sourceType': 'module'
   },
   'plugins': [
-    'react', 'jest', 'cypress'
+    'react', 'jest'
   ],
   'rules': {
     'indent': [
